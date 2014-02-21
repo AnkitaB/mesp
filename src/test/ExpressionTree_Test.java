dfa36c283f2b4a4f25683606aeb4a18e953ac552
Ankita Bhatia <ankitabhatia@dhcp-18-203-32.nat.wireless.uic.edu>
2014-02-20 21:07:52 -0600

    Coverage 55%

--- src/test/ExpressionTree_Test.java
+++ src/test/ExpressionTree_Test.java
@@ -25,7 +25,7 @@ public class ExpressionTree_Test {
 			FuncMap fm = null; // no functions in expression
 
 			System.out.println(x); // (pi*(r^2))
-			//System.out.println(x.eval(vm, fm)); // 78.53981633974483
+			
 			double result= x.eval(vm, fm);
 			double delta = 0.0009;
 			Assert.assertEquals(78.53981633974483, result, delta);
@@ -34,9 +34,170 @@ public class ExpressionTree_Test {
 			fail("Not yet implemented");
 		}
 
-		//vm.setValue("r", 10);
-		//System.out.println(x.eval(vm, fm)); // 314.1592653589793
+		
+		
+	}
+	
+	@Test
+	public void addSubNumbersTest() {
+		try {
+			String s = "x-y+z";
+			Expression x = ExpressionTree.parse(s);
+
+			VarMap vm = new VarMap(false);
+			vm.setValue("x",5);
+			vm.setValue("y", 4);
+			vm.setValue("z", 2);
+
+			FuncMap fm = null; // no functions in expression
+
+			
+			double result= x.eval(vm, fm);
+			double delta = 0.0;
+			System.out.println(result);
+			Assert.assertEquals(3, result, delta);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			fail("Not yet implemented");
+		}
+	}
+		
+		@Test
+		public void powerTest() {
+			try {
+				String s = "(1 + x)^2";
+				Expression x = ExpressionTree.parse(s);
+
+				VarMap vm = new VarMap(false);
+				vm.setValue("x",5);
+				
+				FuncMap fm = null; // no functions in expression
+
+				
+				double result= x.eval(vm, fm);
+				double delta = 0.0;
+				System.out.println(result);
+				Assert.assertEquals(36.0, result, delta);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				fail("Not yet implemented");
+			}
+		
+	}
+		
+
+		@Test
+		public void maxTest() {
+			try {
+				String s = "max(-10,0,20)";
+				Expression x = ExpressionTree.parse(s);
+
+				VarMap vm = new VarMap(false);
+				//vm.setValue("pi",Math.PI);
+				
+				FuncMap fm = new FuncMap(); // max func
+				fm.loadDefaultFunctions();
+				
+				System.out.println("In max");
+				
+				double result= x.eval(vm, fm);
+				double delta = 0.001;
+				System.out.println("Max Result:"+result);
+				Assert.assertEquals(20, result, delta);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				fail("Not yet implemented");
+			}
 		
 	}
 
+
+		@Test
+		public void sineTest() {
+			try {
+				String s = "sin(x)+sin(y)";
+				Expression x = ExpressionTree.parse(s);
+
+				VarMap vm = new VarMap(false);
+				vm.setValue("x",90);
+				vm.setValue("y",45);
+				
+				FuncMap fm = new FuncMap(); // no functions in expression
+				fm.loadDefaultFunctions();
+				
+				
+				
+				System.out.println("In sine");
+				
+				double result= x.eval(vm, fm);
+				double delta = 0.001;
+				System.out.println("Max Result:"+result);
+				Assert.assertEquals(1.74490018813, result, delta);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println(e);
+				fail("Not yet implemented");
+			}
+		
+	}
+		
+
+		@Test
+		public void avgTest() {
+			try {
+				String s = "avg(5.0,10.0,15.0)";
+				Expression x = ExpressionTree.parse(s);
+
+				VarMap vm = new VarMap(false);
+				vm.setValue("x",5.0);
+				vm.setValue("x",10.0);
+				vm.setValue("x",15.0);
+				
+				FuncMap fm = new FuncMap(); // no functions in expression
+				fm.loadDefaultFunctions();
+				
+				
+				
+				System.out.println("In avg");
+				
+				double result= x.eval(vm, fm);
+				double delta = 0.001;
+				System.out.println("Max Result:"+result);
+				Assert.assertEquals(10, result, delta);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println(e);
+				fail("Not yet implemented");
+			}
+		
+	}
+		
+		@Test
+		public void logTest() {
+			try {
+				String s = "log(a+b)";
+				Expression x = ExpressionTree.parse(s);
+
+				VarMap vm = new VarMap(false);
+				vm.setValue("a",10);
+				vm.setValue("b", 20);
+				
+				FuncMap fm = new FuncMap(); // no functions in expression
+				fm.loadDefaultFunctions();
+				
+				
+				
+				System.out.println("In sine");
+				
+				double result= x.eval(vm, fm);
+				double delta = 0.001;
+				System.out.println("Max Result:"+result);
+				Assert.assertEquals(1.47712125, result, delta);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println(e);
+				fail("Not yet implemented");
+			}
+		
+	}
 }
