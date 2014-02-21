cb71edd3c7c79e47dd1bebc157ac63f052cce36e
Ankita Bhatia <ankitabhatia@Ankitas-MacBook-Pro.local>
2014-02-21 12:46:01 -0600

    Coverage 62.6%

--- src/test/ExpressionTree_Test.java
+++ src/test/ExpressionTree_Test.java
@@ -115,12 +115,13 @@ public class ExpressionTree_Test {
 		@Test
 		public void sineTest() {
 			try {
-				String s = "sin(x)+sin(y)";
+				String s = "sin(x)+sin(y+z)";
 				Expression x = ExpressionTree.parse(s);
 
 				VarMap vm = new VarMap(false);
 				vm.setValue("x",90);
 				vm.setValue("y",45);
+				vm.setValue("z", 30);
 				
 				FuncMap fm = new FuncMap(); // no functions in expression
 				fm.loadDefaultFunctions();
@@ -132,7 +133,7 @@ public class ExpressionTree_Test {
 				double result= x.eval(vm, fm);
 				double delta = 0.001;
 				System.out.println("Max Result:"+result);
-				Assert.assertEquals(1.74490018813, result, delta);
+				Assert.assertEquals(0.50621502819, result, delta);
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				System.out.println(e);
@@ -145,13 +146,11 @@ public class ExpressionTree_Test {
 		@Test
 		public void avgTest() {
 			try {
-				String s = "avg(5.0,10.0,15.0)";
+				String s = "avg(5.0,10.0,15.0)+min(10,20)";
 				Expression x = ExpressionTree.parse(s);
 
 				VarMap vm = new VarMap(false);
-				vm.setValue("x",5.0);
-				vm.setValue("x",10.0);
-				vm.setValue("x",15.0);
+			
 				
 				FuncMap fm = new FuncMap(); // no functions in expression
 				fm.loadDefaultFunctions();
@@ -162,8 +161,8 @@ public class ExpressionTree_Test {
 				
 				double result= x.eval(vm, fm);
 				double delta = 0.001;
-				System.out.println("Max Result:"+result);
-				Assert.assertEquals(10, result, delta);
+				System.out.println("Avg Result:"+result);
+				Assert.assertEquals(20, result, delta);
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				System.out.println(e);
@@ -175,24 +174,26 @@ public class ExpressionTree_Test {
 		@Test
 		public void logTest() {
 			try {
-				String s = "log(a+b)";
+				String s = "log(a+b)+log(c)/log(d)";
 				Expression x = ExpressionTree.parse(s);
 
 				VarMap vm = new VarMap(false);
 				vm.setValue("a",10);
 				vm.setValue("b", 20);
+				vm.setValue("c", 10);
+				vm.setValue("d", 20);
 				
 				FuncMap fm = new FuncMap(); // no functions in expression
 				fm.loadDefaultFunctions();
 				
 				
 				
-				System.out.println("In sine");
+				System.out.println("In Log");
 				
 				double result= x.eval(vm, fm);
 				double delta = 0.001;
-				System.out.println("Max Result:"+result);
-				Assert.assertEquals(1.47712125, result, delta);
+				System.out.println("Log Result:"+result);
+				Assert.assertEquals(2.24574304156, result, delta);
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				System.out.println(e);
@@ -200,4 +201,217 @@ public class ExpressionTree_Test {
 			}
 		
 	}
+		
+		@Test
+		public void powTest() {
+			try {
+				String s = "pow(a,b)-pow(c,d)";
+				Expression x = ExpressionTree.parse(s);
+
+				VarMap vm = new VarMap(false);
+				vm.setValue("a",5);
+				vm.setValue("b", 2);
+				vm.setValue("c", 2);
+				vm.setValue("d", 5);
+				
+				FuncMap fm = new FuncMap(); // no functions in expression
+				fm.loadDefaultFunctions();
+				
+				
+				
+				System.out.println("In Pow");
+				
+				double result= x.eval(vm, fm);
+				double delta = 0.00;
+				System.out.println("Pow Result:"+result);
+				Assert.assertEquals(-7.0, result, delta);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println(e);
+				fail("Not yet implemented");
+			}
+		
+	}
+		
+		@Test
+		public void factorialTest() {
+			try {
+				String s = "fact(a*b-c)";
+				Expression x = ExpressionTree.parse(s);
+
+				VarMap vm = new VarMap(false);
+				vm.setValue("a",3);
+				vm.setValue("b", 2);
+				vm.setValue("c", 1);
+				
+				FuncMap fm = new FuncMap(); // no functions in expression
+				fm.loadDefaultFunctions();
+				
+				
+				
+				System.out.println("In Factorial");
+				
+				double result= x.eval(vm, fm);
+				double delta = 0.00;
+				System.out.println("Factorial Result:"+result);
+				Assert.assertEquals(120, result, delta);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println(e);
+				fail("Not yet implemented");
+			}
+		
+	}
+		
+		@Test
+		public void ceilTest() {
+			try {
+				String s = "ceil(a*b+c-d)";
+				Expression x = ExpressionTree.parse(s);
+
+				VarMap vm = new VarMap(false);
+				vm.setValue("a",3.2);
+				vm.setValue("b", 2.7);
+				vm.setValue("c", 1.8);
+				vm.setValue("d", 18.9);
+				
+				FuncMap fm = new FuncMap(); // no functions in expression
+				fm.loadDefaultFunctions();
+				
+				
+				
+				System.out.println("In Celing");
+				
+				double result= x.eval(vm, fm);
+				double delta = 0.00;
+				System.out.println("Ceiling Result:"+result);
+				Assert.assertEquals(-8.00, result, delta);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println(e);
+				fail("Not yet implemented");
+			}
+		
+	}
+		
+		@Test
+		public void sumOfSqrtTest() {
+			try {
+				String s = "sum(sqrt(a),b,c)";
+				Expression x = ExpressionTree.parse(s);
+
+				VarMap vm = new VarMap(false);
+				vm.setValue("a",4);
+				vm.setValue("b", -2);
+				vm.setValue("c", 7);
+				
+				
+				FuncMap fm = new FuncMap(); // no functions in expression
+				fm.loadDefaultFunctions();
+				
+				
+				
+				System.out.println("In Sum of Square Root");
+				
+				double result= x.eval(vm, fm);
+				double delta = 0.00;
+				System.out.println("Sum of Sqrt Result:"+result);
+				Assert.assertEquals(7.0, result, delta);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println(e);
+				fail("Not yet implemented");
+			}
+		
+	}
+
+
+@Test
+public void cosSineHTest() {
+	try {
+		String s = "cosh( x ) + sinh ( y )";
+		Expression x = ExpressionTree.parse(s);
+
+		VarMap vm = new VarMap(false);
+		vm.setValue("x",90);
+		vm.setValue("y", 45);
+		
+		
+		
+		FuncMap fm = new FuncMap(); // no functions in expression
+		fm.loadDefaultFunctions();
+		
+		
+		
+		System.out.println("In Cosh + Sinh");
+		
+		double result= x.eval(vm, fm);
+		double delta = 0.001;
+		System.out.println("Sinh + Cosh:"+result);
+		Assert.assertEquals(6.102016471589175E38, result, delta);
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		System.out.println(e);
+		fail("Not yet implemented");
+	}
+
+}
+
+@Test
+public void expTest() {
+	try {
+		String s = "(exp(x)+exp(-x))/2";
+		Expression x = ExpressionTree.parse(s);
+
+		VarMap vm = new VarMap(false);
+		vm.setValue("x",10);
+		
+		FuncMap fm = new FuncMap(); // no functions in expression
+		fm.loadDefaultFunctions();
+		
+		
+		
+		System.out.println("In exp");
+		
+		double result= x.eval(vm, fm);
+		double delta = 0.001;
+		System.out.println("Exp:"+result);
+		Assert.assertEquals(11013.232920103324, result, delta);
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		System.out.println(e);
+		fail("Not yet implemented");
+	}
+
+}
+
+@Test
+public void roundTest() {
+	try {
+		String s = "round(a)+round(b+c)";
+		Expression x = ExpressionTree.parse(s);
+
+		VarMap vm = new VarMap(false);
+		vm.setValue("a",-10.5);
+		vm.setValue("b",5.8);
+		vm.setValue("c",3.4);
+		
+		FuncMap fm = new FuncMap(); // no functions in expression
+		fm.loadDefaultFunctions();
+		
+		
+		
+		System.out.println("In Round");
+		
+		double result= x.eval(vm, fm);
+		double delta = 0.001;
+		System.out.println("Round:"+result);
+		Assert.assertEquals(-1.0, result, delta);
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		System.out.println(e);
+		fail("Not yet implemented");
+	}
+
+}
 }
